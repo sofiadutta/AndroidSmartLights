a6f6def75614c6086e377f71b4803433b33a5d95
Sofia Dutta <sofiad1@umbc.edu>
2020-05-14 01:34:33 -0400

    lazygit commit 2020-05-14-01-34-33

---
+++ app/src/main/java/com/sofiadutta/SHACApplication.java
@@ -0,0 +1,34 @@
+package com.sofiadutta;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+
+@SuppressLint("Registered")
+public class SHACApplication extends Application {
+    private static final String ADULT_FAMILY_MEMBER = "Adult/Family_Member";
+    private static final String CHILD_FAMILY_MEMBER = "Child/Family_Member";
+    private static final String NON_FAMILY_MEMBER = "Stranger";
+    private static final String USER_INFO = "UserInfo";
+
+    public static String getAdultFamilyMember() {
+        return ADULT_FAMILY_MEMBER;
+    }
+
+    public static String getChildFamilyMember() {
+        return CHILD_FAMILY_MEMBER;
+    }
+
+    public static String getNonFamilyMember() {
+        return NON_FAMILY_MEMBER;
+    }
+
+    public static String getUserInfo() {
+        return USER_INFO;
+    }
+
+    public static String getKasaInfoObject() {
+        return KASA_INFO_OBJECT;
+    }
+
+    private static final String KASA_INFO_OBJECT = "KasaInfoObject";
+}
